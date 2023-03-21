7f702464f5869b29b65f9bbc68eac54fca705ea3
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-03-21 11:08:43 +0100

    promises

---
+++ 03_14_JS/main.mjs
@@ -0,0 +1,11 @@
+import { test } from "./js/prom.mjs";
+// import * as fu from "./js/funcs.mjs";
+
+// fu.test();
+
+window.onload = function () {
+    console.log("On load", new Date());
+
+    test();
+}
+
