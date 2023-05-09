be4d1f72d46c723e9a923612ef98cd705a698590
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2023-05-09 11:25:43 +0200

    array of objects

--- 05_02_ThreeJS_Intro/app.mjs
+++ 05_02_ThreeJS_Intro/app.mjs
@@ -2,12 +2,12 @@ import * as THREE from '../99_Lib/three.module.min.js';
 console.log("ThreeJs " + THREE.REVISION);
 
 let geometries = [
-    new THREE.BoxBufferGeometry(0.25, 0.25, 0.25),
-    new THREE.ConeBufferGeometry(0.2, 0.4, 64),
-    new THREE.CylinderBufferGeometry(0.2, 0.2, 0.2, 64),
-    new THREE.IcosahedronBufferGeometry(0.2, 3),
+    new THREE.BoxGeometry(0.25, 0.25, 0.25),
+    new THREE.ConeGeometry(0.1, 0.4, 64),
+    new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
+    new THREE.IcosahedronGeometry(0.2, 3),
     new THREE.TorusKnotGeometry(.2, .03, 50, 16),
-    new THREE.TorusBufferGeometry(0.2, 0.04, 64, 32)
+    new THREE.TorusGeometry(0.2, 0.04, 64, 32)
 ];
 
 function randomMaterial() {
@@ -40,7 +40,7 @@ window.onload = function () {
     camera.position.set(0, 0, 1);
     scene.add(camera);
     // 
-    let box = new THREE.Mesh(new THREE.BoxBufferGeometry(0.2, 0.2, 0.2), new THREE.MeshStandardMaterial({
+    let box = new THREE.Mesh(new THREE.BoxBufferGeometry(0.1, 0.1, 0.1), new THREE.MeshStandardMaterial({
         color: 0xff3333,
         roughness: 0.7,
         metalness: 0.0,
@@ -48,15 +48,36 @@ window.onload = function () {
     scene.add(box);
     // 
 
+    let arr = [];
+    let count = 0;
+
+    const delta = 0.3, z = -1;
+    for (let x = -1; x <= 1; x += delta) {
+        for (let y = -1; y <= 1; y += delta) {
+            ++count;
+            if (count % 2 == 0)
+                arr.push(add(1, scene, x, y, z));
+            else
+                arr.push(add(4, scene, x, y, z));
+        }
+
+    }
+
+
     let renderer = new THREE.WebGLRenderer({
         antialias: true,
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
     function render() {
         box.rotation.x += 0.01;
         box.rotation.y += 0.01;
+        for (let o of arr) {
+            o.rotation.x += 0.01;
+            o.rotation.y += 0.01;
+        }
         renderer.render(scene, camera);
     }
     renderer.setAnimationLoop(render);
